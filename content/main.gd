020890512387a9421d31c0068370a0054d94d193
Nitwel <mail@nitwel.de>
2023-11-12 17:36:23 +0100

    add light model and brightness slider

--- content/main.gd
+++ content/main.gd
@@ -5,6 +5,8 @@ var sky_passthrough = preload("res://assets/materials/sky_passthrough.material")
 
 @onready var environment: WorldEnvironment = $WorldEnvironment
 @onready var camera: XRCamera3D = $XROrigin3D/XRCamera3D
+@onready var controller_left = $XROrigin3D/XRControllerLeft
+@onready var controller_right = $XROrigin3D/XRControllerRight
 @onready var house = $House
 
 func _ready():
@@ -26,7 +28,11 @@ func _process(delta):
 		camera_basis.x = camera_basis.x.normalized()
 		camera_basis.z = camera_basis.z.normalized()
 
-		camera.position += camera_basis * vector_key_mapping(KEY_D, KEY_A, KEY_S, KEY_W) * delta
+		var movement = camera_basis * vector_key_mapping(KEY_D, KEY_A, KEY_S, KEY_W) * delta
+
+		camera.position += movement
+		controller_left.position += movement
+		controller_right.position += movement
 		
 
 func vector_key_mapping(key_positive_x: int, key_negative_x: int, key_positive_y: int, key_negative_y: int):
