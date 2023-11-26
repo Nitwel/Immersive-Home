1d06821736e6b70d997154d3ce71620523897eec
Nitwel <mail@nitwel.de>
2023-11-26 02:01:27 +0100

    add media player

--- content/ui/menu/edit/entity_creator.gd
+++ content/ui/menu/edit/entity_creator.gd
@@ -3,6 +3,7 @@ extends Object
 const Switch = preload("res://content/entities/switch/switch.tscn")
 const Light = preload("res://content/entities/light/light.tscn")
 const Sensor = preload("res://content/entities/sensor/sensor.tscn")
+const MediaPlayer = preload("res://content/entities/media_player/media_player.tscn")
 
 static func create_entity(type: String, id: String):
 	var entity = null
@@ -14,6 +15,8 @@ static func create_entity(type: String, id: String):
 			entity = Light.instantiate()
 		"sensor":
 			entity = Sensor.instantiate()
+		"media_player":
+			entity = MediaPlayer.instantiate()
 		_:
 			return null
 
