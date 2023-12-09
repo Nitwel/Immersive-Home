46cd29c477aa4fcfeac5b7cef5cdb1883e45261a
Nitwel <mail@nitwel.de>
2023-12-10 00:34:54 +0100

    add button entity

--- content/ui/menu/edit/entity_creator.gd
+++ content/ui/menu/edit/entity_creator.gd
@@ -5,6 +5,7 @@ const Light = preload("res://content/entities/light/light.tscn")
 const Sensor = preload("res://content/entities/sensor/sensor.tscn")
 const MediaPlayer = preload("res://content/entities/media_player/media_player.tscn")
 const Camera = preload("res://content/entities/camera/camera.tscn")
+const ButtonEntity = preload("res://content/entities/button/button.tscn")
 
 static func create_entity(type: String, id: String):
 	var entity = null
@@ -20,8 +21,10 @@ static func create_entity(type: String, id: String):
 			entity = MediaPlayer.instantiate()
 		"camera":
 			entity = Camera.instantiate()
+		"button":
+			entity = ButtonEntity.instantiate()
 		_:
 			return null
-
+			
 	entity.entity_id = id
 	return entity
