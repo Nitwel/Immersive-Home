5fc788d1e35d05569732d2728542d4c76de2dda6
Nitwel <mail@nitwel.de>
2024-05-28 14:42:49 +0200

    add collisions to settings panels

--- app/content/entities/light/settings.gd
+++ app/content/entities/light/settings.gd
@@ -1,4 +1,4 @@
-extends Node3D
+extends StaticBody3D
 
 const Light = preload ("./light.gd")
 
--- app/content/entities/line_chart/settings.gd
+++ app/content/entities/line_chart/settings.gd
@@ -1,4 +1,4 @@
-extends Node3D
+extends StaticBody3D
 
 const LineChart = preload ("./line_chart.gd")
 
